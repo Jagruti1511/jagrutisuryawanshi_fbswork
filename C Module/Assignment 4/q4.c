a807caa01ccaa4e5fedf4a0ea817391d60bfeeb5
Jagruti1511 <jsuryawanshi864@gmail.com>
2026-01-29 16:55:15 +0530

    Add files via upload

---
+++ C Module/Assignment 4/q4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int range;
+	printf("Enter range");
+	scanf("%d",&range);
+	for(int i=1;i<=range;i++){
+		int temp=i,sum=0;
+		while(temp!=0){
+			int rem=temp%10;
+			int fact=1;
+			for(int j=1;j<=rem;j++){
+				fact=fact*j;
+			}
+			sum=sum+fact;
+			temp=temp/10;
+		}
+		if(i==sum)
+			printf("\n %d strong number",i);
+		
+}
+}
