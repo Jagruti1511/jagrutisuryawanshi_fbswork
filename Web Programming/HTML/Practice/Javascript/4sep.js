93fb99528b433dd2b1085109b89c0a9b73517e23
Jagruti Suryawanshi <jsuryawanshi864@gmail.com>
2026-09-04 11:19:06 +0530

    Added JavaScript practice

---
+++ Web Programming/HTML/Practice/Javascript/4sep.js
@@ -0,0 +1,42 @@
+let arr=[10,20,30,40,50]
+arr.forEach(function(ele,index){
+    if(ele==20){
+        console.log("Found at "+index);
+    }
+})
+
+
+for (let ele of arr){
+    console.log(ele);
+}
+
+
+
+let student={
+    name:"Jagruti",
+    age:21,
+    city:"Pune"
+}
+for(let key in strudent){
+    console.log(key+"="+student[key]);
+}
+
+
+let students=[{
+    name:"Jagruti",
+    age:21,
+    city:"Pune"
+},{
+     name:"Rushi",
+    age:22,
+    city:"Pune"
+},{
+     name:"Dhiraj",
+    age:21,
+    city:"Pune"
+}]
+for(let student of students){
+   for(let key in student){
+    console.log(key+"="+student[key]);
+}
+}
