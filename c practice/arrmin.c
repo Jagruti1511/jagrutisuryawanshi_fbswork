cfa56b4cfaf5bf9aaa9efc4af6074dac8d86ba55
Jagruti Suryawanshi <jsuryawanshi864@gmail.com>
2026-09-15 19:07:25 +0530

    Added modulate practice

--- c practice/arrmin.c
+++ c practice/arrmin.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
+
 void main(){
-	int a[5];
-	int min=a[0];
-	
-	printf("Enter the 5 number");
-	for(int i=1;i<5;i++){
-		scanf("%d",&a[i]);
-	}
-	for(int i=1;i<5;i++){
-		if(min<a[i]){
-			min=a[i];
-		}
-		printf("%d",min);
-	}
+    int arr[5];
+    int min;
+
+    printf("Enter the 5 number: ");
+
+    for(int i=0;i<5;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    min=arr[0];
+
+    for(int i=1;i<5;i++){
+        if(min>arr[i]){
+            min=arr[i];
+        }
+    }
+
+    printf("%d",min);
 }
